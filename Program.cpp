c2f4c0ede55045ae08dab65650504a97cc5bb9df
richellemaria <135358640+richellemaria@users.noreply.github.com>
2026-01-13 15:16:23 +0530

    Update and rename Program.cpp to Program.cs

--- Program.cpp
+++
@@ -1,8 +0,0 @@
-using namespace System;
-
-int main(array<String^>^ args)
-{
-    Console::WriteLine("Hello from C++/CLI (.NET)");
-    Console::WriteLine("Args count: {0}", args->Length);
-    return 0;
-}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+// See aka.ms for more information
+Console.WriteLine("Hello, World!");
+
+// You can add more functionality below this line.
+Console.WriteLine("This is a simple .NET 8 application.");
+
+// Example of a simple loop
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Loop iteration: {i}");
+}
