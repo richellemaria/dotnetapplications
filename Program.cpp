03b5066c59db7a4451ace49a653fdbc85f13be1a
richellemaria <135358640+richellemaria@users.noreply.github.com>
2026-01-13 14:35:11 +0530

    Add basic C++/CLI program with sum output

---
+++ Program.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace System;
+
+int main(array<String^>^ args)
+{
+    Console::WriteLine("Hello from C++ .NET (C++/CLI) application!");
+
+    int a = 5;
+    int b = 7;
+    Console::WriteLine("Sum: {0}", a + b);
+
+    return 0;
+}
